47a2ac122e3f974c702dabd8b423175911a64c9b
wobilras <54vovchik54@mail.ru>
2023-02-14 21:47:46 +0300

    second

--- app/src/main/java/com/example/taskss/MainActivity.java
+++ app/src/main/java/com/example/taskss/MainActivity.java
@@ -3,12 +3,66 @@ package com.example.taskss;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = "ApplicationMessage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "Created", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"Created");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(getApplicationContext(), "Started", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"Started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(getApplicationContext(), "Resumed", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"Resumed");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(getApplicationContext(), "Paused", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"Paused");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(getApplicationContext(), "Stopped", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"Stopped");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Toast.makeText(getApplicationContext(), "Restarted", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"Restarted");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(getApplicationContext(), "Destroyed", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"Destroyed");
     }
 }
