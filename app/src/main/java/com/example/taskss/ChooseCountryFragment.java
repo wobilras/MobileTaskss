9492a8d742655e768e98f27a72a0e3d22d2f961b
wobilras <54vovchik54@mail.ru>
2023-03-10 01:59:21 +0300

    list for country finished

--- app/src/main/java/com/example/taskss/ChooseCountryFragment.java
+++ app/src/main/java/com/example/taskss/ChooseCountryFragment.java
@@ -1,45 +1,62 @@
 package com.example.taskss;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.util.ArrayMap;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ChooseCountryFragment extends Fragment {
+    public String TAG = "list";
     interface OnFragmentSendDataListener {
         void onSendData(String data);
     }
+    //private ChooseCountryFragment.OnFragmentSendDataListener fragmentSendDataListener;
 
-    private ChooseCountryFragment.OnFragmentSendDataListener fragmentSendDataListener;
     String[] countries = getResources().getStringArray(R.array.Countries);
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        fragmentSendDataListener = (ChooseCountryFragment.OnFragmentSendDataListener) context;
+       // fragmentSendDataListener = (ChooseCountryFragment.OnFragmentSendDataListener) context;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_choose, container, false);
-        ListView countriesList = view.findViewById(R.id.countryList);
-        ArrayAdapter<String> adapter = new ArrayAdapter(getContext(), android.R.layout.activity_list_item, countries);
+        List<HashMap<String,String>> lst = new ArrayList<HashMap<String,String>>();
+        for(int i=0;i<10;i++){
+            HashMap<String, String> hm = new HashMap<String,String>();
+            hm.put("txt", countries[i]);
+            hm.put("flag", Integer.toString(R.drawable.aiga_toilet_men) );
+            lst.add(hm);
+        }
+        String[] from = { "flag","txt" };
+        int[] to = { R.id.flag, R.id.txt};
+        SimpleAdapter adapter = new SimpleAdapter(getContext(),lst,R.layout.list_layout,from, to);
+        ListView countriesList = ( ListView ) view.findViewById(R.id.countryList);
         countriesList.setAdapter(adapter);
         countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id)
             {
                 String selectedItem = (String)parent.getItemAtPosition(position);
-
+                Log.d(TAG, selectedItem);
+                Toast.makeText(getContext(),selectedItem,Toast.LENGTH_SHORT).show();
                 //fragmentSendDataListener.onSendData(selectedItem);
             }
         });
