231a743c0fdc06777ff83fc4d94412d6bf60145c
wobilras <54vovchik54@mail.ru>
2023-05-12 18:06:19 +0300

    added animated mask to fragment_about, but don't used (use agree arrow)

--- app/src/main/java/com/example/taskss/ui/AboutTheApp.java
+++ app/src/main/java/com/example/taskss/ui/AboutTheApp.java
@@ -1,27 +1,26 @@
 package com.example.taskss.ui;
 
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.taskss.R;
 
 public class AboutTheApp extends Fragment {
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about_the_app, container, false);
+    public AboutTheApp() {
+        super(R.layout.fragment_about_the_app);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ImageView iv = view.findViewById(R.id.maskAbout);
+        AnimatedVectorDrawable avd = (AnimatedVectorDrawable) iv.getDrawable();
+        avd.start();
     }
 }
